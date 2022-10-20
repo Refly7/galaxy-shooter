198c45d361d3a0235caaf3194c10d84ec67c2fcf
Refly7 <108993489+Refly7@users.noreply.github.com>
2022-10-20 18:35:57 +0200

    Update ship.py

--- ship.py
+++ ship.py
@@ -1,40 +1,47 @@
 from missiles import Missiles
 import turtle
-import os
 
+hp = 3
+xpos = 0
+ypos = -200
+max = 480
+up = 90
+down = 270
+right = 0
+left = 180
+speed = 8
+dis_to_bullet = 20
 
-os.chdir("C:\\Users\\Lenovo\\Desktop\\Mój Folder\\Python\\100 dni\\galaxiga")
 turtle.register_shape("ship.gif")
-
 
 class Ship(turtle.Turtle):
 
     def __init__(self):
         turtle.Turtle.__init__(self, shape="ship.gif")
         self.penup()
-        self.setposition(0, -200)
-        self.hp = 3
+        self.setposition(xpos, ypos)
+        self.hp = hp
         self.speed('fastest')
 
     def up(self):
-        if self.ycor() < 480:
-            self.setheading(90)
-            self.forward(8)
+        if self.ycor() < max:
+            self.setheading(up)
+            self.forward(speed)
         
     def down(self):
-        if self.ycor() > -480:
-            self.setheading(270)
-            self.forward(8)
+        if self.ycor() > -max:
+            self.setheading(down)
+            self.forward(speed)
 
     def right(self):
-        if self.xcor() < 480:
-            self.setheading(0)
-            self.forward(8)
+        if self.xcor() < max:
+            self.setheading(right)
+            self.forward(speed)
 
     def left(self):
-        if self.xcor() > -480:
-            self.setheading(180)
-            self.forward(8)
+        if self.xcor() > -max:
+            self.setheading(left)
+            self.forward(speed)
 
     def shoots(self):
-        Missiles(self.xcor(), self.ycor() + 20, 90, 'goldenrod')
+        Missiles(self.xcor(), self.ycor() + dis_to_bullet, up, 'goldenrod')

89b5d3089676c16af13a4a8f867b7db2a42cf166
Refly7 <108993489+Refly7@users.noreply.github.com>
2022-10-20 18:36:27 +0200

    Update stars.py

--- stars.py
+++ stars.py
@@ -1,28 +1,37 @@
 from turtle import Turtle
 from random import randint
 
+speed = 2
+stretch_len = 0.1
+stretch_wid = 0.1
+h = 270
+minpos = -500
+maxpos = 500
+num = 120
+
+
 class Stars():
 
     def __init__(self):
         self.stars = []
-        self.speed = 2
+        self.speed = speed
 
-        for i in range(120):
+        for i in range(num):
             self.newstar()
 
 
     def newstar(self):
         star = Turtle(shape='circle')
         star.penup()
-        star.shapesize(stretch_len = 0.1, stretch_wid = 0.1)
+        star.shapesize(stretch_len, stretch_wid)
         star.color('lightyellow')
-        star.setposition(randint(-500, 500),randint(-500, 500))
-        star.setheading(270)
+        star.setposition(randint(minpos, maxpos), randint(minpos, maxpos))
+        star.setheading(h)
         self.stars.append(star)
 
 
     def move(self):
         for star in self.stars:
             star.forward(self.speed)
-            if star.ycor() < -500:
-                star.setposition(star.xcor(), 500)
+            if star.ycor() < minpos:
+                star.setposition(star.xcor(), maxpos)

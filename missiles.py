fd4dde949024386df2106541f6bff58f179a00d9
Refly7 <108993489+Refly7@users.noreply.github.com>
2022-10-22 18:17:46 +0200

    Update missiles.py

--- missiles.py
+++ missiles.py
@@ -5,12 +5,17 @@ stretch_len = 0.5
 stretch_wid = 0.5
 max = 505
 dis_to_bullet = 20
+dis_to_bullet_boss = 60
 dmg = 1
+speed = 15
+diraction = 270
+lvl = 3
+h = 90
 
 
 class Missiles(turtle.Turtle):
 
-    def __init__(self, x, y, b, c):
+    def __init__(self, x, y, c, b = diraction, speed = speed):
         turtle.Turtle.__init__(self)
 
         self.shape      ('circle')
@@ -18,21 +23,26 @@ class Missiles(turtle.Turtle):
         self.color      (c)
         self.shapesize  (stretch_len, stretch_wid)
         self.setheading (b)
+        self.speed = speed
         self.setposition(x, y)
         allbullets.append(self)
 
 
     def move(self):
-        self.forward(15)
+        self.forward(self.speed)
         
-        if -max > self.ycor() > max:
+        if -max > self.ycor() or self.ycor() > max:
             self.erase()
 
 
     def collision(self, obj):
-        if self.distance(obj) < dis_to_bullet:
+        if self.distance(obj) < dis_to_bullet or obj.lvl == lvl and self.distance(obj) < dis_to_bullet_boss and self.heading() == h:
             self.erase()
+            obj.hideturtle()
             obj.hp -= dmg
+            return True
+
+        return False
 
     
     def erase(self):
